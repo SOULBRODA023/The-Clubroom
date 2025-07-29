5e315a2ac606d31d9e7b2ef9e509d37f24188f37
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2025-07-29 08:18:05 -0600

    started again

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-	console.log("Redirected to /");
-	res.render("index", { user: req.user });
-});
-
-module.exports = router;
