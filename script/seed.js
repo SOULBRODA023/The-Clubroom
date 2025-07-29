3be860a92cba9675d78fa5532b55626d427cf8d4
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2025-07-29 00:33:27 -0600

    another seed

--- script/seed.js
+++ script/seed.js
@@ -1,56 +1,47 @@
-const { Client } = require("pg");
-require("dotenv").config();
-
-const client = new Client({
-	connectionString: process.env.DATABASE_URL,
-});
+const pool = require("../server/model/pool"); 
 
 async function seed() {
 	console.log("starting seeding...");
 	try {
-		await client.connect();
+		await pool.query(`
+			CREATE TABLE IF NOT EXISTS users (
+				id SERIAL PRIMARY KEY,
+				firstname VARCHAR(255) NOT NULL,
+				lastname VARCHAR(255) NOT NULL,
+				email VARCHAR(255) UNIQUE NOT NULL,
+				password TEXT NOT NULL,
+				is_member BOOLEAN DEFAULT FALSE,
+				is_admin BOOLEAN DEFAULT FALSE
+			);
+		`);
 
-		await client.query(`
-      CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        firstname VARCHAR(255) NOT NULL,
-        lastname VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password TEXT NOT NULL, 
-        is_member BOOLEAN DEFAULT FALSE,
-        is_admin BOOLEAN DEFAULT FALSE
-      );
-    `);
+		await pool.query(`
+			CREATE TABLE IF NOT EXISTS messages (
+				id SERIAL PRIMARY KEY,
+				title VARCHAR(255) NOT NULL,
+				text TEXT NOT NULL,
+				created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+				user_id INTEGER,
+				CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+			);
+		`);
 
-		await client.query(`
-      CREATE TABLE IF NOT EXISTS messages(
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        text TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        user_id INTEGER,
-        CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
-      );
-    `);
+		await pool.query(`
+			CREATE TABLE IF NOT EXISTS user_sessions (
+				sid VARCHAR NOT NULL PRIMARY KEY,
+				sess JSON NOT NULL,
+				expire TIMESTAMP NOT NULL
+			);
+		`);
 
-		await client.query(`
-      CREATE TABLE IF NOT EXISTS user_sessions (
-        sid VARCHAR NOT NULL PRIMARY KEY,
-        sess JSON NOT NULL,
-        expire TIMESTAMP NOT NULL
-      );
-    `);
-
-		await client.query(`
-      CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "user_sessions" ("expire");
-    `);
+		await pool.query(`
+			CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "user_sessions" ("expire");
+		`);
 
 		console.log("Tables created successfully");
 	} catch (err) {
 		console.error("seed error:", err);
-	} finally {
-		await client.end();
 	}
 }
 
-module.exports = seed; 
+module.exports = seed;
